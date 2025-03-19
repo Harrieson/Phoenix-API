9ea95e0f32496d06fddde81af131d3c75fe83076
Harrieson <harriemburu@gmail.com>
2025-03-19 21:03:57 +0300

    Error responses for Guarded routes

--- lib/real_deal_api_web/auth/error_response.ex
+++ lib/real_deal_api_web/auth/error_response.ex
@@ -1,3 +1,6 @@
 defmodule RealDealApiWeb.Auth.ErrorResponse.Unauthorized do
   defexception message: "Unauthorized", plug_status: 401
 end
+defmodule RealDealApiWeb.Auth.ErrorResponse.Forbidden do
+  defexception [message: "You do have permisions to view this page.", plug_status: 403]
+end
